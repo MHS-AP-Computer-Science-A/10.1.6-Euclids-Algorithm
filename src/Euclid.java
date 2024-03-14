3a4e6791fe946a8877cd68f8514de5a2f3d85a81
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-03-14 11:55:23 -0400

    Add files via upload

---
+++ src/Euclid.java
@@ -0,0 +1,20 @@
+public class Euclid {
+
+
+        public static void main(String[] args) {
+            System.out.println("GCD of " + 18 + " and " + 48 + " is: " + gcd(18, 48));
+            System.out.println("GCD of " + 48 + " and " + 18 + " is: " + gcd(48, 18));
+            System.out.println("GCD of " + 5 + " and " + 10 + " is: " + gcd(5, 10));
+            System.out.println("GCD of " + 210 + " and " + 49 + " is: " + gcd(210, 49));
+
+
+
+        }
+    
+        public static int gcd(int a, int b) {
+
+       
+        }
+    
+    
+}
