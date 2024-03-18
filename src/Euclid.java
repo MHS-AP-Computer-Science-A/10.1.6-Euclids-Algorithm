541400ecf545168db3e523c2289bd601159ece49
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-03-18 09:40:29 -0400

    Update Euclid.java

--- src/Euclid.java
+++ src/Euclid.java
@@ -2,10 +2,10 @@ public class Euclid {
 
 
         public static void main(String[] args) {
-            System.out.println("GCD of " + 18 + " and " + 48 + " is: " + gcf(18, 48));
-            System.out.println("GCD of " + 48 + " and " + 18 + " is: " + gcf(48, 18));
-            System.out.println("GCD of " + 5 + " and " + 10 + " is: " + gcf(5, 10));
-            System.out.println("GCD of " + 210 + " and " + 49 + " is: " + gcf(210, 49));
+            System.out.println("GCF of " + 18 + " and " + 48 + " is: " + gcf(18, 48));
+            System.out.println("GCF of " + 48 + " and " + 18 + " is: " + gcf(48, 18));
+            System.out.println("GCF of " + 5 + " and " + 10 + " is: " + gcf(5, 10));
+            System.out.println("GCF of " + 210 + " and " + 49 + " is: " + gcf(210, 49));
 
 
 
